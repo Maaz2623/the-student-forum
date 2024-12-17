ad09f31282acd9a3650b07e49c7144e479f23c25
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2024-12-18 00:02:59 +0530

    modified:   bun.lockb 	modified:   package.json 	new file:   src/app/(auth)/layout.tsx 	new file:   src/app/(auth)/sign-in/[[...sign-in]]/page.tsx 	new file:   src/app/(auth)/sign-up/[[...sign-up]]/page.tsx 	new file:   src/app/(dashboard)/events/loading.tsx 	new file:   src/app/(home)/_components/auth-dialog.tsx 	modified:   src/app/(home)/_components/navbar.tsx 	modified:   src/app/layout.tsx 	modified:   src/components/convex-client-provider.tsx 	new file:   src/components/ui/dialog.tsx 	modified:   src/middleware.ts

--- src/app/(home)/_components/navbar.tsx
+++ src/app/(home)/_components/navbar.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import { Teko } from "next/font/google";
-import { UserButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignIn, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./auth-dialog";
+import { Button } from "@/components/ui/button";
 
 const teko = Teko({
   subsets: ["latin"],
@@ -10,7 +19,7 @@ const teko = Teko({
 
 const Navbar = () => {
   return (
-    <div className="border-b bg-slate-100 border-gray-150 h-14 px-4 py-3 flex justify-between items-center">
+    <div className="border-gray-150 h-14 px-4 py-3 flex justify-between items-center">
       <div className={`flex items-center justify-center gap-x-1`}>
         <Image
           src={`/logo.svg`}
@@ -23,8 +32,25 @@ const Navbar = () => {
           The Student Forum
         </p>
       </div>
+
       <div className="">
-        <UserButton />
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <SignedOut>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant={`secondary`}>Sign in</Button>
+            </DialogTrigger>
+            <DialogContent className="flex justify-center items-center bg-transparent border-none shadow-none">
+              <DialogHeader>
+                <DialogTitle></DialogTitle>
+                <DialogDescription></DialogDescription>
+              </DialogHeader>
+              <SignIn signUpFallbackRedirectUrl={`/events`} routing="hash" />
+            </DialogContent>
+          </Dialog>
+        </SignedOut>
       </div>
     </div>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ConvexClientProvider } from "@/components/convex-client-provider";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,17 +15,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`antialiased`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <ConvexClientProvider>{children}</ConvexClientProvider>
-        </ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider
+      afterSignOutUrl={`/`}
+      signInFallbackRedirectUrl={`/events`}
+      signUpFallbackRedirectUrl={`/events`}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <body className={`antialiased`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <ConvexClientProvider>{children}</ConvexClientProvider>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
