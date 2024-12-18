cb496c33f4d1888b71d52d67fb2bf016072f4db2
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2024-12-18 13:25:19 +0530

    deleted:    public/bg-1.svg 	modified:   src/app/(home)/_components/navbar.tsx 	modified:   src/app/(home)/page.tsx

--- src/app/(home)/_components/navbar.tsx
+++ src/app/(home)/_components/navbar.tsx
@@ -33,7 +33,7 @@ const Navbar = () => {
   }
 
   return (
-    <div className="border-gray-150 h-14 px-4 py-3 flex border-b justify-between items-center">
+    <div className="border-gray-150 dark:border-gray-800 h-14 px-4 py-3 flex border-b justify-between items-center">
       <div className={`flex items-center justify-center gap-x-1`}>
         <Image
           src={`/logo.svg`}
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,17 +1,11 @@
+import Link from "next/link";
 import React from "react";
 
 const HomePage = () => {
   return (
-    <div
-      className=""
-      style={{
-        backgroundImage: "url('/bg-1.svg')",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        minHeight: "100vh",
-      }}
-    ></div>
+    <div className="">
+      <Link href={`/events`}>Events</Link>
+    </div>
   );
 };
 
