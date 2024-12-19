9a19cc63bc8c23f61c17517b287e5645dd69fc5b
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2024-12-20 00:30:23 +0530

    modified:   src/app/(dashboard)/events/page.tsx

--- src/app/(dashboard)/events/page.tsx
+++ src/app/(dashboard)/events/page.tsx
@@ -1,7 +1,22 @@
+import { Separator } from "@/components/ui/separator";
 import React from "react";
 
 const EventsPage = () => {
-  return <div>EventsPage</div>;
+  return (
+    <div>
+      <header>
+        <h1 className="text-2xl font-medium">Events</h1>
+        <p className="text-sm text-neutral-600">Join us in the events and enjoy!</p>
+      </header>
+      <Separator className="my-8" />
+
+      <div className="border aspect-video w-[300px]">
+        <div className="">
+
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default EventsPage;
